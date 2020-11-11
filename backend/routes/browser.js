de0925b13f19dd01edce9952c969583a88075b50
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2020-11-11 07:28:49 -0600

    Design for tables finished.

--- backend/routes/browser.js
+++ backend/routes/browser.js
@@ -6,4 +6,8 @@ router.get('/login', (req, res) => {
   res.sendFile('login.html', { root: 'views' });
 });
 
+router.get('/contacts', (req, res) => {
+  res.sendFile('contacts.html', { root: 'views' });
+});
+
 module.exports = router;
